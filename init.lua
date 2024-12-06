3f7d36be064c2cf94bf71e5e5ba59f21ad325ff4
mrkuz <markus@bitsandbobs.net>
2024-12-06 17:53:59 +0100

    Switch to alacritty

--- init.lua
+++ init.lua
@@ -128,7 +128,7 @@ shellActions:registerAction({
 shellActions:registerAction({
       { 'hyper' }, 't', modal = hyper,
       text = 'New terminal',
-      command = 'kitty'
+      command = 'alacritty'
 })
 
 appleScriptActions:registerAction({
